bd93fb817748bae59cfab7ede64feaf91eb84eed
ParkInseon <insunlike@naver.com>
2022-07-29 19:59:07 +0900

    Refactor: header 중복 처리를 위한 코드 분리

---
+++ js/header.js
@@ -0,0 +1,55 @@
+document.write(`
+    <!-- Mobile Menu -->
+    <div class="mobile-navigation">
+        <div class="mobile">
+            <div class="mobile__logo">
+                <h1 class="logo"><a href="">SERENA</a></h1>
+                <button id="closeMenuBtn" class="mobile__button"><i class="fa-solid fa-xmark"></i></button>
+            </div>
+            <nav>
+                <ul class="mobile__menu">
+                    <li><a href="">home</a></li>
+                    <li><a href="">shop</a></li>
+                    <li><a href="">about</a></li>
+                    <li><a href="">details</a></li>
+                    <li><a href="">photos</a></li>
+                    <li><a href="">blog</a></li>
+                    <li><a href="">contact</a></li>
+                </ul>
+            </nav>
+            <ul class="mobile__sns">
+                <li><a href=""><i class="fa-brands fa-instagram-square"></i></a></li>
+                <li><a href=""><i class="fa-brands fa-facebook-f"></i></a></li>
+                <li><a href=""><i class="fa-brands fa-pinterest"></i></a></li>
+                <li><a href=""><i class="fa-brands fa-twitter"></i></a></li>
+            </ul>
+        </div>
+    </div>
+    <!-- Header GNB-->
+    <div class="header-box">
+        <header class="header">
+            <div class="header__logo">
+                <h1 class="logo"><a href="">SERENA</a></h1>
+                <button id="menuBtn" class="navigation-btn"><i class="fa-solid fa-bars"></i></button>
+            </div>
+            <nav class="gnb">
+                <ul class="gnb__menu">
+                    <li><a href="">shop</a></li>
+                    <li><a href="">about</a></li>
+                    <li><a href="">details</a></li>
+                    <li><a href="">photos</a></li>
+                    <li><a href="">blog</a></li>
+                    <li><a href="">contact</a></li>
+                </ul>
+            </nav>
+        </header>
+        <!-- Main Section -->
+        <section class="main layout">
+            <div class="contents main__contents">
+                <p class="main__title">Write a short pitch about who you are and what you do </p>
+                <div class="main__more"><a class="main__button button button--theme-white" href="">learn more</a></div>
+            </div>
+            <div class="bg main__img"></div>
+        </section>                                                                                                                                                                                                                                            
+    </div>
+`);
