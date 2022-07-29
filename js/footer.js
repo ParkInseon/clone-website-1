b50ada847bcb1479a4bc54db73fee6e710af2cb9
ParkInseon <insunlike@naver.com>
2022-07-29 19:58:39 +0900

    Refactor: footer 중복 처리를 위한 코드 분리

---
+++ js/footer.js
@@ -0,0 +1,20 @@
+document.write(`
+    <footer class="footer">
+        <ul class="footer__photo">
+            <li><img src="../img/main.jpg" alt=""></li>
+            <li><img src="../img/yoga.jpg" alt=""></li>
+            <li><img src="../img/studio.jpg" alt=""></li>
+        </ul>
+        <ul class="footer__sns">
+            <li><a href=""><i class="fa-brands fa-instagram-square"></i></a></li>
+            <li><a href=""><i class="fa-brands fa-facebook-f"></i></a></li>
+            <li><a href=""><i class="fa-brands fa-pinterest"></i></a></li>
+            <li><a href=""><i class="fa-brands fa-twitter"></i></a></li>
+        </ul>
+        <ul class="footer__text">
+            <li><a href="">buy this template</a></li>
+            <li><a href="">design by the roar</a></li>
+            <li><a href="">photos unsplash</a></li>
+        </ul>
+    </footer>
+`);
