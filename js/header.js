661a8d0d53c12cf910bef3f1a6ca77e0fcd2ea81
ParkInseon <insunlike@naver.com>
2022-08-03 16:05:00 +0900

    Add contact page and image files

--- js/header.js
+++ js/header.js
@@ -3,18 +3,18 @@ document.write(`
     <div class="mobile-navigation">
         <div class="mobile">
             <div class="mobile__logo">
-                <h1 class="logo"><a href="">SERENA</a></h1>
+                <h1 class="logo"><a href="../html/home.html">SERENA</a></h1>
                 <button id="closeMenuBtn" class="mobile__button"><i class="fa-solid fa-xmark"></i></button>
             </div>
             <nav>
                 <ul class="mobile__menu">
-                    <li><a href="">home</a></li>
-                    <li><a href="">shop</a></li>
+                    <li><a href="../html/home.html">home</a></li>
+                    <li><a href="../html/shop.html">shop</a></li>
                     <li><a href="">about</a></li>
                     <li><a href="">details</a></li>
                     <li><a href="">photos</a></li>
                     <li><a href="">blog</a></li>
-                    <li><a href="">contact</a></li>
+                    <li><a href="../html/contact.html">contact</a></li>
                 </ul>
             </nav>
             <ul class="mobile__sns">
@@ -29,17 +29,17 @@ document.write(`
     <div class="header-box">
         <header class="header">
             <div class="header__logo">
-                <h1 class="logo"><a href="">SERENA</a></h1>
+                <h1 class="logo"><a href="../html/home.html">SERENA</a></h1>
                 <button id="menuBtn" class="navigation-btn"><i class="fa-solid fa-bars"></i></button>
             </div>
             <nav class="gnb">
                 <ul class="gnb__menu">
-                    <li><a href="">shop</a></li>
+                    <li><a href="../html/shop.html">shop</a></li>
                     <li><a href="">about</a></li>
                     <li><a href="">details</a></li>
                     <li><a href="">photos</a></li>
                     <li><a href="">blog</a></li>
-                    <li><a href="">contact</a></li>
+                    <li><a href="../html/contact.html">contact</a></li>
                 </ul>
             </nav>
         </header>                                                                                                                                                                                                                                        
